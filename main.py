f962d0532f158b248c136aa33b7b6833a5755b42
Saulo Costa <saulotarsobc@gmail.com>
2022-01-31 09:01:46 -0300

    \

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import readCsv
-import zabbixApi
+import zabbix
 
-AUTHTOKEN = zabbixApi.login()
-
-hosts = readCsv.getHosts('./hosts.csv')
-
-for i in hosts:
-    print(f"{i['nome']} => {i['ip']}")
+file_csv = 'hosts.csv'
+criar_grupo = ""
+id_grupo = ""
 
 
-zabbixApi.logout(AUTHTOKEN)
+""" Logar no zabbix """
+AUTHTOKEN = zabbix.login()
+
+
+""" Ler arquivo csv """
+hosts = readCsv.getHosts(f"./{file_csv}")
+
+
+""" Criar grupo ? """
+while criar_grupo == "":
+    criar_grupo = (input('Criar novo grupo? S | N : ').upper())
+
+
+""" Criar grupo """
+if criar_grupo == 'S':
+    while id_grupo == "":
+        nome_grupo = (input('Qual o nome do novo grupo?: '))
+        res = zabbix.criarGrupo(AUTHTOKEN, nome_grupo)
+        if 'error' in res:
+            print(f'\nErro: {res["error"]["data"]}\n')
+        else:
+            id_grupo = res['result']['groupids'][0]
+            print(f'\nO grupo "{nome_grupo}" com ID "{id_grupo}" foi criado\n')
+
+""" Listar Grupos existentes """
+if criar_grupo == 'N':
+    while id_grupo == "":
+        grupos = zabbix.getGroups(AUTHTOKEN)
+        for grupo in grupos['result']:
+            print(f'{grupo["groupid"]} => {grupo["name"]}')
+        id_grupo = input('\nInsira o ID do Grupo: ')
+        print(f'\nGrupo: {id_grupo}\n')
+
+print(hosts)
+
+""" Deslogar """
+zabbix.logout(AUTHTOKEN)
--- zabbix.py
+++ zabbix.py
@@ -1,6 +1,6 @@
+from configparser import ConfigParser
 import requests
 import json
-from configparser import ConfigParser
 
 config = ConfigParser()
 config.read('./config.ini')
@@ -27,11 +27,46 @@ def login():
         print(f"Nao consegui logar")
 
 
+def criarGrupo(AUTHTOKEN, nome_grupo):
+    try:
+        r = requests.post(url, json={
+            "jsonrpc": "2.0",
+            "method": "hostgroup.create",
+            "params": {
+                "name": nome_grupo
+            },
+            "auth": AUTHTOKEN,
+            "id": 2
+        })
+        res = r.json()
+        return res
+    except Exception:
+        print(f"\nerror\n")
+
+
+def getGroups(AUTHTOKEN):
+    try:
+        r = requests.post(url, json={
+            "jsonrpc": "2.0",
+            "method": "hostgroup.get",
+            "params": {
+                "output": "extend",
+                "status": 0
+            },
+            "auth": AUTHTOKEN,
+            "id": 1
+        })
+        res = r.json()
+        return res
+    except Exception:
+        print(f"\nerror\n")
+
+
 def logout(AUTHTOKEN):
     if (AUTHTOKEN):
         try:
             r = requests.post(url, json={"jsonrpc": "2.0", "method": "user.logout", "params": {
-            }, "id": 2, "auth": AUTHTOKEN})
+            }, "id": 100, "auth": AUTHTOKEN})
             print(f"\n>>>>>>>>>> Logout: {json.dumps(r.json()['result'])}")
         except Exception as err:
             print("logout: ", err)
